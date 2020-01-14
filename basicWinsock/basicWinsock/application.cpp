f78a028bcf8f42b6f896ef7b6486d746dab2b029
Smallyellowface <jctheathlete@gmail.com>
2020-01-13 18:52:03 -0800

    init commit

---
+++ basicWinsock/basicWinsock/application.cpp
@@ -0,0 +1,215 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE: winmain.cpp -	An application that creates a window and process window messages
+--								that define the behavior of dumb terminal.
+--
+--
+-- PROGRAM: DumbTerminalEmulation
+--
+-- FUNCTIONS:
+--				WinMain(HINSTANCE hInst, HINSTANCE hprevInstance,
+--						LPSTR lspszCmdParam, int nCmdShow)
+--				LRESULT CALLBACK WndProc(HWND hwnd, UINT Message,
+--						WPARAM wParam, LPARAM lParam)
+--				void PrintToScreen(WPARAM wParam)
+--				DWORD WINAPI MonitorInputThread(LPVOID hwnd)
+--
+-- DATE: September 21, 2019
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jameson Cheong
+--
+-- PROGRAMMER: Jameson Cheong
+--
+-- NOTES:
+-- This is a minimal terminator emulator that performs basic functions of
+-- connection to a serial port and transmitting or receiving characters from the port.
+-- Also, it provides user interface for user to set port settings and see displayed transmitted char.
+----------------------------------------------------------------------------------------------------------------------*/
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <Windows.h>
+#include "application.h"
+
+BOOL comPortSet = FALSE;
+PORTPARMA portparma;
+HDC hdc;
+static unsigned k = 0;
+static TCHAR Name[] = TEXT("Dumb Terminal");
+char str[255];	//output buffer
+int X = 0, Y = 0; // Current coordinates
+
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: WinMain
+--
+-- DATE: September 21, 2019
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jameson Cheong
+--
+-- PROGRAMMER: Jameson Cheong
+--
+-- INTERFACE: int WinMain(HINSTANCE hInst, HINSTANCE hprevInstance,
+--					LPSTR lspszCmdParam, int nCmdShow)
+--
+-- RETURNS: int
+--
+-- NOTES:
+-- This function creates window and the user interface.
+--
+----------------------------------------------------------------------------------------------------------------------*/
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hprevInstance,
+	LPSTR lspszCmdParam, int nCmdShow)
+{
+	MSG Msg;
+	WNDCLASSEX Wcl;
+	HBRUSH startBackGroundColor = CreateSolidBrush(RGB(236, 240, 241));
+	// Define a Window class
+	Wcl.cbSize = sizeof(WNDCLASSEX);
+	Wcl.style = 0; // default style
+	Wcl.hIcon = LoadIcon(NULL, (LPCSTR)IDI_APPLICATION); // large icon 
+	Wcl.hIconSm = NULL; // use small version of large icon
+	Wcl.hCursor = LoadCursor(NULL, IDC_ARROW);  // cursor style
+
+	Wcl.lpfnWndProc = WndProc; // window function
+	Wcl.hInstance = hInst; // handle to this instance
+	Wcl.hbrBackground = startBackGroundColor;
+	Wcl.lpszClassName = Name; // window class name
+
+	Wcl.lpszMenuName = TEXT("MYMENU");
+	Wcl.cbClsExtra = 0;      // no extra memory needed
+	Wcl.cbWndExtra = 0;
+
+	//intialize readThread = 0;
+	portparma.readThread = 0;
+
+	// Register the class
+	if (!RegisterClassEx(&Wcl))
+		return 0;
+
+	portparma.hwnd = CreateWindow(Name, Name, WS_OVERLAPPEDWINDOW, 10, 10,
+		600, 400, NULL, NULL, hInst, NULL);
+	ShowWindow(portparma.hwnd, nCmdShow);
+	UpdateWindow(portparma.hwnd);
+	void WINAPI ThreadFuc(HWND hwnd, LPVOID n); //second thread
+	// Create the message loop
+	while (GetMessage(&Msg, NULL, 0, 0))
+	{
+		TranslateMessage(&Msg); // translate keybpard messages
+		DispatchMessage(&Msg); // dispatch message and return control to windows
+	}
+
+	return Msg.wParam;
+}
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: WndProc
+--
+-- DATE: September 21, 2019
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jameson Cheong
+--
+-- PROGRAMMER: Jameson Cheong
+--
+-- INTERFACE: LRESULT CALLBACK WndProc(HWND hwnd, UINT Message,
+--						WPARAM wParam, LPARAM lParam)
+--
+-- RETURNS: LRESULT
+--
+-- NOTES:
+-- This function process windows messages and define the behavior of the dumb terminal.
+--
+----------------------------------------------------------------------------------------------------------------------*/
+LRESULT CALLBACK WndProc(HWND hwnd, UINT Message,
+	WPARAM wParam, LPARAM lParam)
+{
+
+	char charByte[2];
+	hdc = GetDC(hwnd);
+	TCHAR spCOM1[] = TEXT("COM1");
+	TCHAR spCOM2[] = TEXT("COM2");
+	TCHAR spCOM3[] = TEXT("COM3");
+
+
+	switch (Message)
+	{
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		}
+
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+
+			break;
+		}
+		break;
+	case WM_CHAR:	// Process keystroke
+
+		break;
+	case WM_DESTROY:		// message to terminate the program
+		PostQuitMessage(0);
+		break;
+	default: // Let Win32 process all other messages
+		return DefWindowProc(hwnd, Message, wParam, lParam);
+	}
+	return 0;
+}
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: PrintToScreen
+--
+-- DATE: Spetember 24, 2019
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jameson Cheong
+--
+-- PROGRAMMER: Jameson Cheong
+--
+-- INTERFACE: void printToScreen(char readBuffer[])
+--
+-- RETURNS: void.
+--
+-- NOTES:
+-- This function prints characters on the screen.
+----------------------------------------------------------------------------------------------------------------------*/
+
+void PrintToScreen(WPARAM wParam) {
+
+	SIZE lastCharWidth;
+	//hdc = GetDC(portparma.hwnd);
+	RECT windowDimension;
+	char strKey[2];
+
+	//finds width of the window
+	GetWindowRect(portparma.hwnd, &windowDimension);
+	int windowWidth = windowDimension.right - windowDimension.left;
+
+	//get the width of the last character read
+	sprintf_s(strKey, "%c", (char)wParam);
+	GetTextExtentPoint32(hdc, strKey, 1, &lastCharWidth);
+
+	//print the characters
+	SetBkMode(hdc, TRANSPARENT);
+	TextOut(hdc, X, Y, strKey, 1);
+
+	//adds the screen paint coordinates by the amount of the last char width
+	X += lastCharWidth.cx;
+
+	//if the displayed characters exceed window width, jump to the next line
+	if (X > windowWidth - 20) {
+		X = 0;
+		Y += 15;
+	}
+	ReleaseDC(portparma.hwnd, hdc); // Release device context
+}
+
---
+++ basicWinsock/basicWinsock/application.h
@@ -0,0 +1,45 @@
+#pragma once
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE: WinMain.h - contains the declaration functions
+--							and libraries for this project
+--
+--
+--
+-- PROGRAM: DumbTerminalEmulation
+--
+-- DATE: September 21, 2019
+--
+-- REVISIONS:
+--
+-- DESIGNER: Jameson Cheong
+--
+-- PROGRAMMER: Jameson Cheong
+--
+-- NOTES:
+-- This is a header file contains declaration functions and libraries.
+----------------------------------------------------------------------------------------------------------------------*/
+#pragma once
+#pragma warning (disable: 4096)
+#define _CRT_SECURE_NO_WARNINGS
+
+#define STRICT
+#include <windows.h>
+
+#define IDM_HELP						100
+#define IDM_EXIT						105
+
+
+//Declaration Functions application
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void PrintToScreen(WPARAM wParam);
+
+
+typedef struct
+{
+	HWND hwnd;
+	HANDLE hSerial;
+	HANDLE readThread;
+	DCB dcb;
+	BOOL connectMode;
+	COMMCONFIG cc;
+} PORTPARMA;
